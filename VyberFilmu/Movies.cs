c6ce7e86ba67a3602b60b6fb2d178d4cdc532526
Veronika Šošolíková <veronika.sosolikova@gmail.com>
2018-01-13 16:12:22 +0100

    Uklid - vymazani prebytecneho, formatovani

--- VyberFilmu/Movies.cs
+++ VyberFilmu/Movies.cs
@@ -1,60 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.IO;
-using System.Threading;
-
-
-
 
 
 namespace VyberFilmu
 {
     class Movies
     {
-        private List<Movie> movies;
-        private Dictionary<string,string> unikatniSeznamZanru = new Dictionary<string, string>();
+
+        private Dictionary<string, string> unikatniSeznamZanru = new Dictionary<string, string>();
         private Dictionary<string, string> unikatniSeznamZemi = new Dictionary<string, string>();
         private Random losovani = new Random();
         private DatabazeFilmu databaze;
 
-        public Movies()
-        {
-            movies = new List<Movie>();
-        }
-        public void PridatFilm()
-        {
-            Console.WriteLine("ZADÁNÍ NOVÉHO FILMU");
-            Console.Write("Nazev: ");
-            string name = Console.ReadLine();
-            Console.Write("Country: ");
-            string country = Console.ReadLine();
-            Console.Write("Genre 1: ");
-            string genre1 = Console.ReadLine();
-            Console.Write("Genre 2: ");
-            string genre2 = Console.ReadLine();
-            Console.Write("Genre 3: ");
-            string genre3 = Console.ReadLine();
-            List<string> listGenre = new List<string>
-            {
-                genre1,
-                genre2,
-                genre3
-            };
-            Movie movie = new Movie(name, country, listGenre);
-            movies.Add(movie);
-        }
-        public void VypisFilm()
-        {
-            Console.WriteLine("VYPIS FILMU");
-            foreach (Movie movie in movies)
-            {
-                movie.VypisPopis();
-            }
-        }
+
         public void NactiFilmyZeSouboru()
         {
             using (StreamReader r = new StreamReader("Movies.json"))
@@ -66,7 +27,7 @@ namespace VyberFilmu
                 foreach (Movie item in databaze.Movies)
                 {
 
-                    
+
                     foreach (string genre in item.Genre)
                     {
                         if (!unikatniSeznamZanru.ContainsKey(genre))
@@ -80,7 +41,7 @@ namespace VyberFilmu
                         unikatniSeznamZemi.Add(item.Country, item.Country);
                     }
                 }
-            }          
+            }
         }
 
         public List<string> NahodnyVyberZemi()
@@ -96,7 +57,7 @@ namespace VyberFilmu
         public List<string> NahodnyVyberZanru()
         {
             List<string> listNahodnych = new List<string>();
-            
+
             var nahodneCislo = losovani.Next(unikatniSeznamZanru.Count);
             listNahodnych.Add(unikatniSeznamZanru.ToArray()[nahodneCislo].Value);
             nahodneCislo = losovani.Next(unikatniSeznamZanru.Count);
@@ -115,7 +76,7 @@ namespace VyberFilmu
 
             }
             if (listFilmuSplnujicichPodminky.Count < 1)
-            {               
+            {
                 return null;
             }
             else
@@ -123,9 +84,6 @@ namespace VyberFilmu
                 var nahodneCislo = losovani.Next(listFilmuSplnujicichPodminky.Count);
                 return listFilmuSplnujicichPodminky[nahodneCislo];
             }
-            
-            
-            
         }
 
         public class DatabazeFilmu
--- VyberFilmu/Program.cs
+++ VyberFilmu/Program.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
-using System.IO;
-using System.Threading;
+
 
 namespace VyberFilmu
 {
@@ -15,9 +10,6 @@ namespace VyberFilmu
         {
             Movies movies = new Movies();
             movies.NactiFilmyZeSouboru();
-           
-            
-            
 
             var vylosovaneZeme = movies.NahodnyVyberZemi();
             var vylosovanyZanr = movies.NahodnyVyberZanru();
@@ -34,30 +26,6 @@ namespace VyberFilmu
                 Console.WriteLine("{0} {1} žánry: {2}", filmNahodneZvolenyDleUzivatelskychParametru.Name, filmNahodneZvolenyDleUzivatelskychParametru.Country, String.Join(", ", filmNahodneZvolenyDleUzivatelskychParametru.Genre));
             }
 
-            int volbaMenu = 0;
-            do
-            {
-                Console.WriteLine("HLAVNI MENU");
-                Console.WriteLine("\t 1. Pridat film");
-                Console.WriteLine("\t 2. Vypsat filmy");
-                Console.WriteLine("\t 3. Nacti filmy ze souboru");
-                Console.WriteLine("\t 4. Ukoncit program");
-
-                volbaMenu = Int32.Parse(Console.ReadLine());
-                switch (volbaMenu)
-                {
-                    case 1:
-                        movies.PridatFilm();
-                        break;
-                    case 2:
-                        movies.VypisFilm();
-                        break;
-                    case 3:
-                        movies.NactiFilmyZeSouboru();
-                        break;
-                }
-
-            } while (volbaMenu != 4);
             Console.ReadKey();
         }
         
